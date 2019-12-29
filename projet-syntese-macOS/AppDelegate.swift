7bce82de7a3df11241f3f51ac807ac54206ba3ff
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2019-12-29 15:21:06 +0100

    Initial Commit

---
+++ projet-syntese-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  projet-syntese-macOS
+//
+//  Created by M'haimdat omar on 29-12-2019.
+//  Copyright © 2019 M'haimdat omar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ projet-syntese-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  projet-syntese-macOS
+//
+//  Created by M'haimdat omar on 29-12-2019.
+//  Copyright © 2019 M'haimdat omar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
